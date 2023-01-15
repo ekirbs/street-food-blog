7a958adcbc198f7995670a9a7df40cb1142f0009
Eric K <erickirberger@gmail.com>
2023-01-14 22:02:11 -0500

    added some base page work, working on profile not rendering issue...cookies or redirect loop errors.

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,14 @@
+const streetFoodApiKey = "";
+const mapApiKey = "";
+const weatherApiKey = "3044316f6126db93462603440b6cd43c";
+
+
 function init() {
   setInterval(function () {
     $("#currentDay").text(dayjs().format("dddd MMM DD, YYYY [-] h:mm:ss a"));
   }, 1000);
 
   // renderHistory();
-}
+};
 
 init();
