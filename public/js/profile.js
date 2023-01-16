018a8edc8ba13fdf6351a2d10e131ff7fe58f333
Eric K <erickirberger@gmail.com>
2023-01-16 16:51:29 -0500

    commiting for pull

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,13 +2,13 @@ const newFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  // const needed_funding = document.querySelector('#project-funding').value.trim();
+  const description = document.querySelector('#profile-desc').value.trim();
 
-  if (name && needed_funding && description) {
-    const response = await fetch(`/api/projects`, {
+  if (name && description) {
+    const response = await fetch(`/api/profile`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ name, description }),
       headers: {
         'Content-Type': 'application/json',
       },
@@ -17,7 +17,7 @@ const newFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert('Failed to create project');
+      alert('Failed to create profile');
     }
   }
 };
@@ -26,22 +26,22 @@ const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/projects/${id}`, {
+    const response = await fetch(`/api/profile/${id}`, {
       method: 'DELETE',
     });
 
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert('Failed to delete project');
+      alert('Failed to delete profile');
     }
   }
 };
 
 document
-  .querySelector('.new-project-form')
+  .querySelector('.new-profile-form')
   .addEventListener('submit', newFormHandler);
 
 document
-  .querySelector('.project-list')
+  .querySelector('.profile-list')
   .addEventListener('click', delButtonHandler);
