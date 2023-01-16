b62ed68990c0d669f77beb5232a226e198949794
Eric K <erickirberger@gmail.com>
2023-01-16 14:14:41 -0500

    added some basic appendage language and some css changes

--- public/js/scriptSandbox.js
+++ public/js/scriptSandbox.js
@@ -9,6 +9,16 @@ function displayChosenCity() {
     })
     .then(function (data) {
       console.log(data);
+
+      let name = v[1].name;
+      $("#vendor-name").append(`<h3 style="text-decoration: underline">${name}</h3>`);
+
+      let website = v[1].url;
+      $("#vendor-name").append(`<a href="${website}">${website}</a>`);
+
+      let description = v[1].description;
+      $("#vendor-description-card").append("<br/>" + "Vendor Description" + "<br/>" + description + "<br/>");
+
     })
   };
 
